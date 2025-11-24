9950a8dec23b8d3f60e7501351ca761be97408bc
Jonas <jonas.neto19@aluno.ce.gov.br>
2025-11-24 12:50:47 -0300

    adicionar métodos CRUD no EstoqueServico

--- src/main/java/br/com/exemplo/estoque/service/EstoqueServico.java
+++ src/main/java/br/com/exemplo/estoque/service/EstoqueServico.java
@@ -22,4 +22,32 @@ private ProdutoRepositorio pr;
         return pr.findById(id).orElse(null);
     }
 
+    // CADASTRAR
+    public ProdutoModelo cadastrar(ProdutoModelo produto) {
+        return pr.save(produto);
+    }
+
+    // ATUALIZAR
+    public ProdutoModelo atualizar(Long id, ProdutoModelo dados) {
+        ProdutoModelo existente = pr.findById(id).orElse(null);
+
+        if (existente != null) {
+            existente.setNome(dados.getNome());
+            existente.setDescricao(dados.getDescricao());
+            existente.setModelo(dados.getModelo());
+            existente.setPreco(dados.getPreco());
+            return pr.save(existente);
+        }
+
+        return null;
+    }
+
+    // DELETAR
+    public boolean deletar(Long id) {
+        if (pr.existsById(id)) {
+            pr.deleteById(id);
+            return true;
+        }
+        return false;
+    }
 }
